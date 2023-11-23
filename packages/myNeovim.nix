2cf351831ca0a4c2d331ba015150022bd82e5e89
Steven Ulin <steveulin@gmail.com>
2023-11-22 20:21:59 -0800

    Change how runtime deps are added to be more cannonical.

--- packages/myNeovim.nix
+++ packages/myNeovim.nix
@@ -3,18 +3,15 @@ let
   customRC = import ../config { inherit pkgs; };
   plugins = import ../plugins.nix { inherit pkgs; };
   runtimeDeps = import ../runtimeDeps.nix {inherit pkgs; };
-  neovimRuntimeDependencies = pkgs.symlinkJoin {
-    name = "neovimRuntimeDependencies";
-    paths = runtimeDeps;
-  };
   myNeovim = pkgs.wrapNeovim pkgs.neovim {
     configure = {
       inherit customRC;
       packages.all.start = plugins;
     };
   };
-in pkgs.writeShellApplication {
+in pkgs.symlinkJoin {
   name = "nvim";
-  runtimeInputs = [ neovimRuntimeDependencies ];
-  text = ''${myNeovim}/bin/nvim "$@"'';
+  paths = [ myNeovim ] ++ runtimeDeps;
+  buildInputs = [ pkgs.makeWrapper ];
+  postBuild = "wrapProgram $out/bin/nvim --prefix PATH : $out/bin";
 }
