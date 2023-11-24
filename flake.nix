3ba88d372a32bc4fcf1d7040461f3ca2b77ecf7e
Steven Ulin <steveulin@gmail.com>
2023-11-23 20:33:21 -0800

    Use import instead of getFlake

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs-staging.url = "github:NixOS/nixpkgs/staging";
     neovim = {
       url = "github:neovim/neovim?dir=contrib";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -13,25 +14,24 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, neovim, ... }:
+  outputs = { self, nixpkgs, nixpkgs-staging, neovim, ... }:
     let
+      system = "x86_64-linux";
       overlayFlakeInputs = prev: final: {
-        neovim = neovim.packages.x86_64-linux.neovim.override {
-          inherit ((builtins.getFlake
-          "github:NixOS/nixpkgs/d4758c3f27804693ebb6ddce2e9f6624b3371b08").legacyPackages.x86_64-linux)
-          libvterm-neovim;
+        neovim = neovim.packages.${system}.neovim.override {
+          inherit (import nixpkgs-staging { inherit system; }) libvterm-neovim;
         };
       };
       overlayMyNeovim = prev: final: {
         myNeovim = (import ./packages/myNeovim.nix { pkgs = final; });
       };
       pkgs = import nixpkgs {
-        system = "x86_64-linux";
+        inherit system;
         overlays = [ overlayFlakeInputs overlayMyNeovim ];
       };
     in {
-      packages.x86_64-linux.default = pkgs.myNeovim;
-      apps.x86_64-linux.default = {
+      packages.${system}.default = pkgs.myNeovim;
+      apps.${system}.default = {
         type = "app";
         program = "${pkgs.myNeovim}/bin/nvim";
       };
