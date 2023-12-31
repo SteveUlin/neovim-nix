e30735e1351b383d9fe83007a061e765aa9eca09
Steven Ulin <steveulin@gmail.com>
2023-12-31 13:04:22 -0800

    Fix snippets in nvim cmp

--- config/plugins.nix
+++ config/plugins.nix
@@ -26,6 +26,11 @@
 
       gitsigns.enable = true;
 
+      luasnip = {
+        enable = true;
+        fromVscode = [{paths = "${pkgs.vimPlugins.friendly-snippets}";}];
+      };
+
       neorg-exec.enable = true;
 
       neorg = {
@@ -64,6 +69,12 @@
           "<C-Space>" = "cmp.mapping.complete()";
           "<Tab>" = ''
             cmp.mapping(function(fallback)
+              local luasnip = require("luasnip")
+              local has_words_before = function()
+                unpack = unpack or table.unpack
+                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+              end
               if cmp.visible() then
                 cmp.select_next_item()
               -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
@@ -79,6 +90,7 @@
           '';
           "<S-Tab>" = ''
             cmp.mapping(function(fallback)
+              local luasnip = require("luasnip")
               if cmp.visible() then
                 cmp.select_prev_item()
               elseif luasnip.jumpable(-1) then
@@ -92,6 +104,8 @@
           "<C-k>" = "cmp.mapping(cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }), {'i'})";
           "<C-e>" = "cmp.mapping.close()";
         };
+
+        snippet.expand = "luasnip";
 
         sources = [
           { name = "luasnip"; }
