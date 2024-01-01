72c1f7ffdc585c9b575bf6bde141686b9922f814
Steven Ulin <steveulin@gmail.com>
2024-01-01 14:42:19 -0800

    Fix autocommand

--- config/autocmd.nix
+++ config/autocmd.nix
@@ -2,12 +2,15 @@
 autoCmd = [
   {
     event = [ "FileType" ];
-    pattern = [ "*.norg" ];
-    command = '':lua
+    pattern = "norg";
+    callback = {
+      __raw = ''function()
       vim.opt_local.conceallevel = 3
       vim.opt_local.concealcursor = "nc"
       vim.opt_local.wrap = false
+    end
     '';
+    };
   }
 ];
 }
